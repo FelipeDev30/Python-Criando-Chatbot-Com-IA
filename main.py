62728ab473446223df8818f0933c6a3a2484428b
= <=>
2026-07-09 22:45:32 -0300

    Integrado chatbot com Gemini, e adicionado gitignore para seguranca de credenciais

---
+++ main.py
@@ -0,0 +1,63 @@
+# titulo
+# input do chat
+# a cada mensagem enviada:
+    # mostrar a mensagem que o usuario enviou no chat
+    # enviar essa mensagem para a IA responder
+    # aparece na tela a resposta da IA
+
+# streamlit - frontend e backend
+
+import streamlit as st
+from google import genai
+from google.genai import types
+
+api_key = st.secrets["general"]["GEMINI_API_KEY"]
+cliente = genai.Client(api_key=api_key)
+
+st.write("### ChatBot com IA") # markdown
+
+# session_state = memoria do streamlit
+if not "lista_mensagens" in st.session_state:
+    st.session_state["lista_mensagens"] = []
+
+# adicionar uma mensagem
+# st.session_state["lista_mensagens"].append(mensagem)
+
+# exibir o histórico de mensagens
+for mensagem in st.session_state["lista_mensagens"]:
+    role = mensagem["role"]
+    content = mensagem["content"]
+    st.chat_message(role).write(content)
+
+mensagem_usuario = st.chat_input("Escreva sua mensagem aqui")
+
+if mensagem_usuario:
+    # user -> ser humano
+    # assistant -> inteligencia artificial
+    st.chat_message("user").write(mensagem_usuario)
+    mensagem = {"role": "user", "content": mensagem_usuario}
+    st.session_state["lista_mensagens"].append(mensagem)
+
+    # resposta da IA
+    # Converter a lista de mensagens para o formato do Gemini (mudando "assistant" para "model")
+    contents = []
+    for msg in st.session_state["lista_mensagens"]:
+        role = "user" if msg["role"] == "user" else "model"
+        contents.append(
+            types.Content(
+                role=role,
+                parts=[types.Part(text=msg["content"])]
+            )
+        )
+
+    resposta_modelo = cliente.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=contents
+    )
+    
+    resposta_ia = resposta_modelo.text
+
+    # exibir a resposta da IA na tela
+    st.chat_message("assistant").write(resposta_ia)
+    mensagem_ia = {"role": "assistant", "content": resposta_ia}
+    st.session_state["lista_mensagens"].append(mensagem_ia)
